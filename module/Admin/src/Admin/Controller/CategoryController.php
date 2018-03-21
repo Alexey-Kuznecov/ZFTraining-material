f38020906058deda649737581622dfcfedc1464d
Alexsofar <lexa1th@gmail.com>
2018-03-21 15:08:07 +0800

    Create new module for edit category

--- module/Admin/src/Admin/Controller/CategoryController.php
+++ module/Admin/src/Admin/Controller/CategoryController.php
@@ -54,6 +54,69 @@ class CategoryController extends BaseController
         }
 
         return $this->redirect()->toRoute('admin/category');
-    }    
+    } 
+    
+    public function editAction()
+    {
+        $status = $message = '';
+        $em = $this->getEntityManager();    
+        $form = new CategoryAddForm();
+        
+        $id = (int) $this->params()->fromRoute('id', 0);
+
+        $category = $em->find('Blog\Entity\Category', $id);
+        
+        if(empty($category))
+        {
+            $message = 'Категория не найдена';
+            $status = 'error';
+            $this->flashMessenger()
+                    ->setNamespace($status)
+                    ->addMessage($message);
+            return $this->redirect()->toRoute('admin/category');
+        }
+        
+        $form->bind($category);
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) 
+        {
+            $data = $request->getPost();
+            $form->setData($data);
+            
+            if ($form->isValid()) 
+            {             
+                $em->persist($category);
+                $em->flush();
+                
+                $status = 'success';
+                $message = 'Категория обнавлена';
+            } 
+            else 
+            {
+                $status = 'error';
+                $message = 'Ошибка параметров';
+                
+                foreach ($form-getInputFilter()->getInvalidInput() as $error)
+                {
+                    foreach ($error->getMessages() as $error)
+                    {
+                        $message .= ' ' . $error;
+                    }
+                }
+            }
+        } else {
+            return array('form' => $form, 'id' => $id);
+        }
+        
+        if ($message) 
+        {
+            $this->flashMessenger()
+                    ->setNamespace($status)
+                    ->addMessage($message);
+        }
+
+        return $this->redirect()->toRoute('admin/category');
+    }
 }
 
